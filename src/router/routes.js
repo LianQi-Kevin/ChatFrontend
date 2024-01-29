b659c262f2ebf0f4839f933fdf6fca231764b340
LianQi-Kevin <18201584863@163.com>
2024-01-29 20:13:20 +0800

    * 添加 localforage 到依赖 * 简要封装 localforage 相关方法到 src/tools/db.js * 完成 API-Config Card

--- src/router/routes.js
+++ src/router/routes.js
@@ -12,6 +12,15 @@ const routes = [
         }
     },
     {
+        path: '/config',
+        name: 'Config',
+        component: () => import('@/view/Config.vue'),    // lazyLoad
+        meta: {
+            requiresAuth: false,    // 是否需要鉴权 ( 非必要, 默认false )
+            title: 'Config'   // 页面标题 ( 非必要 )
+        }
+    },
+    {
         // 配置全局匹配，跳转到 home
         path: '/:pathMatch(.*)*',
         redirect: '/home'
