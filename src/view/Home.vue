93864eb0fcff0db8e3a5bec3621ea046ca8fc933
LianQi-Kevin <18201584863@163.com>
2024-02-27 15:18:08 +0800

    * 抽取文件上传按钮及相关逻辑到 src/components/UploadBtn.vue (未完成)

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -2,23 +2,24 @@
 // import DB from '@/tools/db.ts';
 import Config from "@/components/ConfigSpark.vue";
 import ChatCard from "@/components/ChatCard.vue"
-import {DocumentAdd, Promotion, Refresh} from "@element-plus/icons-vue";
+import {Promotion, Refresh, Share} from "@element-plus/icons-vue";
 import {listModels} from "@/network/OpenaiApi"
 import {openaiChatCompletionRequestMessages} from "@/types/OpenaiAPITypes";
+import UploadBtn from "@/components/UploadBtn.vue";
+// import type {UploadInstance, UploadProps, UploadRawFile } from "element-plus";
 
-const modelName: Ref<string> = ref('');
-let modelList: {label: string; value: string}[] = []
-const showConfig: Ref<boolean> = ref(false)
-const inputValue: Ref<string> = ref('hello')
+// const upload = ref<UploadInstance>()
+const modelName = ref<string>('');
+let modelList = reactive<{label: string; value: string}[]>([])
+const showConfig = ref<boolean>(false)
+const inputValue = ref<string>('hello')
 
 interface messagesListType extends openaiChatCompletionRequestMessages{
   userName?: string;
 }
 
-const messagesList: Ref<messagesListType[]> = ref([
+const messagesList = ref<messagesListType[]>([
   { role: 'system', userName: 'system', content: 'You are a helpful assistant.'},
-  { role: 'user', content: 'Give me some python Example' },
-  { role: 'assistant', userName: 'Bot', content: 'Sure\n```python\ndoc.sections[0].page_height = Cm(29.7)\n```' },
 ])
 
 
@@ -31,19 +32,17 @@ async function updateApiCredentials() {
 }
 
 function submitMessage() {
-  // todo: 待封装相关请求函数
-  // messagesList.value.push({ role: 'user', content: inputValue.value })
+  messagesList.value.push({ role: 'user', content: inputValue.value })
 
   // inputValue.value = ''
 }
 
 const baseURL = "https://u23218-b635-989ec868.beijinga.seetacloud.com/"
 
-onBeforeMount(() => {
+onMounted(() => {
   nextTick(async () => {
-    modelList = (await listModels(baseURL)).map(item => {return {label: item, value: item}})
-    modelName.value = modelList[0].value
-    // console.log(modelList)
+    (await listModels(baseURL)).map(item => {modelList.push({label: item.split('/')[1], value: item}) })
+    modelName.value = modelList[0]?.value
     // console.log(modelList)
     // await createCompletion(baseURL, [{"role": "user", "content": "hello"}],
     //   "google/gemma-7b-it", {stream: false})
@@ -59,6 +58,7 @@ onBeforeMount(() => {
         <el-select v-model="modelName" placeholder="Model" >
           <el-option v-for="item in modelList" :key="item.label" :label="item.label" :value="item.value" />
         </el-select>
+        <el-button disabled> <el-icon><Share style="transform: scale(1.2);"/></el-icon></el-button>
       </div>
       <div class="conversations">
         <template v-for="{content, role, userName} in messagesList">
@@ -67,12 +67,7 @@ onBeforeMount(() => {
       </div>
       <div class="inputArea">
         <div class="rowTools">
-          <el-button type="info" size="large" link class="uploadBtn">
-            Upload Files
-            <el-icon style="margin-left: 5px" >
-              <DocumentAdd style="transform: scale(1.2);"/>
-            </el-icon>
-          </el-button>
+          <UploadBtn />
           <el-button type="info" size="large" link class="refreshBtn" @click="() => {messagesList = [messagesList[0]]}">
             New Chat
             <el-icon style="margin-left: 5px" >
@@ -134,9 +129,11 @@ onBeforeMount(() => {
       padding: 8px;
       display: flex;
       flex-direction: row;
+      justify-content: space-between;
+
       :deep(.el-select__wrapper){
         box-shadow: none;
-
+        //max-width: 200px;
         font-size: 18px;
         font-weight: 500;
       }
